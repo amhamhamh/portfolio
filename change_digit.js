5b8b07af74229f52041e95b3006a089ce50c30cd
umc-code <51162782+amhamhamh@users.noreply.github.com>
2021-03-30 09:45:59 +0900

    Add files via upload

--- change_digit.js
+++ change_digit.js
@@ -1,11 +1,11 @@
 class Calculator {
-    constructor(print_digit, input_digit, transfer_digit) {
+    constructor(print_digit, input_digit, transfer_digit) { //생성자
         this.print_digit = print_digit;
         this.input_digit = input_digit;
         this.transfer_digit = transfer_digit;
     }
 
-    
+
     make_transfer_digit = () => {
         var transfer_bitwise = $(this.input_digit).val(); //스트링 형태  
         var ten_zinsu_test = /^([0-9]){1,12}$/g;
@@ -15,15 +15,15 @@ class Calculator {
             switch (divN) {
                 //10진수를 16진수로 바꾸기
                 case "16":
-                    let hexMatrix = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+                    let hexMatrix = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
                     let hexA = [];
                     let div_n = 16;
                     var sixteen_zinsu = "";
 
                     function decToHex(inV) {
-                        hexA.unshift(hexMatrix[(inV % div_n) - 1]);
+                        hexA.unshift(hexMatrix[(inV % div_n)]);
                         if (inV < div_n) {
-                            return 0;
+                            return;
                         } else {
                             inV = Math.floor(inV / div_n);
                         }
@@ -84,7 +84,6 @@ class Calculator {
         var test_ten = /^([0-9]){1,12}$/g;
         let divN = $(this.transfer_digit).val();
         var yepp = $(this.input_digit).val();
-
         switch (divN) {
             case "2":
                 test_second.test(yepp) ? $(this.print_digit).text(translate_second(yepp)) : $(this.print_digit).text("2진수를 입력하세요");
@@ -96,10 +95,16 @@ class Calculator {
                 test_sixteen.test(yepp) ? $(this.print_digit).text(translate_sixteen(yepp)) : $(this.print_digit).text("16진수를 입력하세요");
                 break;
             case "10":
-                test_ten.test(yepp) ? $(this.print_digit).text(yepp) : $(this.print_digit).text("10진수를 입력하세요");
+                //test_ten.test(yepp) ? $(this.print_digit).text(yepp) : $(this.print_digit).text("10진수를 입력하세요");
+                test_ten.test(yepp) ? $(this.print_digit).text(remove_ten_zen(yepp)) : $(this.print_digit).text("10진수를 입력하세요");
                 break;
         }
 
+        function remove_ten_zen(arg) {
+            var transefer_bitwise = Number(arg);
+            return transefer_bitwise;
+        }
+        //2진수로 변환
         function translate_second(arg) {
             var transefer_bitwise = arg;
             var conD = 2;
@@ -111,7 +116,7 @@ class Calculator {
             }
             return transefer_ten;
         }
-
+        //8진수로 변환
         function translate_eight(arg) {
             var transefer_bitwise = arg;
             var conD = 8;
@@ -124,7 +129,7 @@ class Calculator {
             }
             return transefer_ten;
         }
-
+        //16진수로 변환
         function translate_sixteen(arg) {
             var transefer_bitwise = arg;
             let hex_matrix = {
@@ -160,82 +165,97 @@ class Calculator {
             return transfer_ten;
         }
     }
-
+    //플러스 연산
     mix_cal_plus = () => {
-        let box1_result = parseInt($("#print_area").html());
-        let box2_result = parseInt($("#print_area1").html());
+        let box1_result = $("#print_area").html();
+        let box2_result = $("#print_area1").html();
         if ($("#get_digit").val() == "" || $("#get_digit1").val() == "") {
-            $("#alert_text").fadeIn(2000,"swing",function(){                
+            $("#alert_text").fadeIn(2000, "swing", function () {
                 $("#alert_text").show();
                 $("#alert_text").hide();
             });
         } else {
-            var input_sum_result = box1_result + box2_result;
-            $("#result_digit").val(input_sum_result);
-        }
-    }
-
-    mix_cal_minus = () => {
-        let box1_result = parseInt($("#print_area").html());
-        let box2_result = parseInt($("#print_area1").html());
-        if ($("#get_digit").val() == "" || $("#get_digit1").val() == "") {
-            $("#alert_text").fadeIn(2000,"swing",function(){                
-                $("#alert_text").show();
-                $("#alert_text").hide();
-            });
-        } else {
-            if (box1_result >= box2_result) {
-                var minus_result = box1_result - box2_result;
-                $("#result_digit").val(minus_result);
-            } else {
-                $("#alert_text1").fadeIn(2000,"linear",function(){                
-                    $("#alert_text1").show();
-                    $("#alert_text1").hide();
-                });
+            if (box1_result > 0 && box2_result > 0) {
+                var input_sum_result = parseInt(box1_result) + parseInt(box2_result);
+                $("#result_digit").val(input_sum_result);
+            } else {                
+                $("#result_digit").text("");
             }
         }
     }
-
-    mix_cal_multiple = () => {
-        let box1_result = parseInt($("#print_area").html());
-        let box2_result = parseInt($("#print_area1").html());
+    //마이너스 연산
+    mix_cal_minus = () => {
+        let box1_result = $("#print_area").html();
+        let box2_result = $("#print_area1").html();
         if ($("#get_digit").val() == "" || $("#get_digit1").val() == "") {
-            $("#alert_text").fadeIn(2000,"swing",function(){                
+            $("#alert_text").fadeIn(2000, "swing", function () {
                 $("#alert_text").show();
                 $("#alert_text").hide();
             });
         } else {
-            var mutiple_result = box1_result * box2_result;
-            $("#result_digit").val(mutiple_result);
+            if (box1_result >= box2_result && box1_result > 0 && box2_result > 0) {
+                var minus_result = parseInt(box1_result) - parseInt(box2_result);
+                $("#result_digit").val(minus_result);
+            } else {
+                if (Number(box1_result) < Number(box2_result)) {
+                    $("#alert_text1").fadeIn(2000, "linear", function () {
+                        $("#alert_text1").show();
+                        $("#alert_text1").hide();
+                    });
+                }
+            }
         }
     }
-
+    //곱하기 연산
+    mix_cal_multiple = () => {
+        let box1_result = $("#print_area").html();
+        let box2_result = $("#print_area1").html();
+        if ($("#get_digit").val() == "" || $("#get_digit1").val() == "") {
+            $("#alert_text").fadeIn(2000, "swing", function () {
+                $("#alert_text").show();
+                $("#alert_text").hide();
+            });
+        } else {
+            if (box1_result > 0 && box2_result > 0) {
+                var mutiple_result = parseInt(box1_result) * parseInt(box2_result);
+                $("#result_digit").val(mutiple_result);
+            } else {                
+                $("#result_digit").text("");
+            }
+        }
+    }
 }
 
 
-$(this).on("click change", function (e) {
+$(this).on("click change", function (e) { //출력구간, 입력숫자, 변환하는 진법
     let transfer_result = new Calculator($("#print_area"), $("#get_digit"), $("#transfer_digit"));
     let transfer_result1 = new Calculator($("#print_area1"), $("#get_digit1"), $("#transfer_digit1"));
     let sum_result = new Calculator($("#print_area2"), $("#result_digit"), $("#transfer_digit2"));
-    let mix_cal = new Calculator($("#result_digit"));    
+    let mix_cal = new Calculator($("#result_digit"));
     switch (e.target.id) {
         case "submit_digit1":
-            transfer_result.transfer_ten_zinsu();
+            transfer_result.transfer_ten_zinsu(); //박스 1값 n진수 입력
             break;
         case "submit_digit2":
-            transfer_result1.transfer_ten_zinsu();
+            transfer_result1.transfer_ten_zinsu(); //박스 2값 n진수 입력
             break;
         case "transfer_digit2":
-            sum_result.make_transfer_digit();
+            sum_result.make_transfer_digit(); //결과값 10진수 입력
             break;
         case "plus":
-            mix_cal.mix_cal_plus();
+            mix_cal.mix_cal_plus(); //플러스 연산
             break;
         case "minus":
-            mix_cal.mix_cal_minus();
+            mix_cal.mix_cal_minus(); //마이너스 연산
             break;
         case "mutiple":
-            mix_cal.mix_cal_multiple();
+            mix_cal.mix_cal_multiple(); //곱하기 연산
             break;
     }
+})
+
+
+
+$(function () {
+    $(".no_space").css("height", window.innerHeight);
 })
